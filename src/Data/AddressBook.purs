152b2d1907dc2bf242237528dd4144f81c2245f5
raghav-kukreti <raghav.kukreti@gmail.com>
2020-04-04 00:49:33 +0530

    Basic addressbook

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -20,6 +20,9 @@ type Address = {
 
 type AddressBook = List Entry  -- List is a type constructor :: takes a type <a> to return an object of type List <a>
 
+emptyBook :: AddressBook
+emptyBook = empty
+
 showEntry :: Entry -> String -- Takes a parameter of type Entry and returns a String
 showEntry entry = entry.firstname <> " " <> entry.lastname <> " " <> showAddress entry.address
 
@@ -27,10 +30,15 @@ showAddress :: Address -> String
 showAddress address = address.street <> ", " <> address.state <> ", " <> address.city
 
 insertEntry :: Entry -> AddressBook -> AddressBook 
--- Example of a curried function; all functions in Purs take one arg
--- Evaluated as Entry -> (AddressBook -> AddressBook)
-insertEntry entry book = Cons entry book -- Cons defn : V a. a -> List a 
+{- 
+    Example of a curried function; all functions in Purs take one arg
+    Evaluated as Entry -> (AddressBook -> AddressBook)
+-}
+insertEntry entry book = Cons entry book -- Cons defn : V a. a -> List a  this can be eta-reduced to just a Cons
 
-
-
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstname lastname book = head $ filter filterEntry book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstname == firstname && entry.lastname == lastname
 
